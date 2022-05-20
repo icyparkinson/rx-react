2610888f25100f9c1f9e106587f36082a7b397b2
koulight <koulight@hotmail.com>
2022-05-19 22:22:44 -0700

    Updated homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import React from "react"
 const Home = () => {
     return(
         <section>
-            <h1 style = {{color: "red"}}>Rx Buddy</h1>
-            <p>Select from options above!</p>
+            
+            <h1 style = {{fontSize: "3rem", textDecoration: "underline"}}>Rx Buddy</h1>
+            <img src = "images/logoblack.png" />
+            <h1 style = {{fontSize: "2rem"}}>Welcome!</h1>
+            <p style = {{margin: "20px"}}>Hi there~ I hope I can help you today. To start, select from one of the options above!</p>
         </section>
+            
+           
+        
     )
 }
 
