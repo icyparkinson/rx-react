4724ee21ade5900ed46f803758fb93cf592fa730
koulight <koulight@hotmail.com>
2022-11-15 09:46:36 -0800

    Updated homepage with contact for suggestions.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <h1 style = {{fontSize: "3rem", textDecoration: "underline", fontFamily: "Fredoka One"}}>Rx Buddy</h1>
             <img src = "images/logoblack.png" alt = "Rx Buddy logo"/>
             <h1 style = {{fontSize: "2rem"}}>Yay!</h1>
-            <p style = {{margin: "20px"}}>Hi there~ I hope I can help you today. To start, select from one of the options above!</p>
+            <p style = {{margin: "20px"}}>Hi there~ I hope I can help you today. To start, select from one of the options above! If you have any suggestions for how to improve or add to Rx Buddy, let Icy know! </p>
             <em>**Use me at your discretion! Rx Buddy takes no responsibility or liability.**</em>
 
         </section>
