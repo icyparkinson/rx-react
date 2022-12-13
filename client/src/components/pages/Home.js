c59f4ba2d1c1a0f24d8d5c8001b072470b8d8c41
koulight <koulight@hotmail.com>
2022-12-12 17:17:03 -0800

    Reformatted homepage with styled components.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,26 @@
 import React from "react"
+import styled from "styled-components"
+
+const TopSection = styled.section`
+    margin-bottom: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 3rem;
+    text-decoration: underline;
+    font-family: "Fredoka One";
+`
 
 const Home = () => {
     return(
-        <section style = {{marginBottom: "50px"}}>
-            
-            <h1 style = {{fontSize: "3rem", textDecoration: "underline", fontFamily: "Fredoka One"}}>Rx Buddy</h1>
+        <TopSection>
+            <Title>Rx Buddy</Title>
             <img src = "images/logoblack.png" alt = "Rx Buddy logo"/>
-            <h1 style = {{fontSize: "2rem"}}>Yay!</h1>
+            <h2 style = {{fontSize: "2rem"}}>Yay!</h2>
             <p style = {{margin: "20px"}}>Hi there~ I hope I can help you today. To start, select from one of the options above!</p>
             <p>If you have any suggestions for how to improve or add to Rx Buddy, let Icy know!</p>
             <em>**Use me at your discretion! Rx Buddy takes no responsibility or liability.**</em>
-
-        </section>
+        </TopSection>
             
            
         
