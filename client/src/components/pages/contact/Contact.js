2e22411313de4fae848c1e45f540adec22cd76d9
koulight <koulight@hotmail.com>
2023-01-03 15:32:56 -0800

    Trying form.

--- client/src/components/pages/contact/Contact.js
+++ client/src/components/pages/contact/Contact.js
@@ -32,7 +32,7 @@ function Contact(){
             <h1>Contact Icy</h1>
                         <p class="reach">If you have any comments, questions, or suggestions for improvements, please let me know!</p>
     
-                        <Form target="_blank" name="rxbuddy" method="POST" data-netlify="true">
+                        {/* <Form target="_blank" name="rxbuddy" method="POST" data-netlify="true">
                             <div>
                             <label for="name">Name</label>
                                 <input type="text" name="name" class="form-control" placeholder="Your Name" required />
@@ -41,7 +41,26 @@ function Contact(){
                             </div>
                                 <textarea placeholder="Your Message" class="form-control" name="message" rows="5" required></textarea>
                           <button>Send Message</button>
-                        </Form>
+                        </Form> */}
+
+                    <form name="rxbuddyContact" method="post">
+                            <input type="hidden" name="rxBuddy-contact" value="rxbuddyContact" />
+                            <p>
+                                <label>Your Name: <input type="text" name="name"/></label>
+                            </p>
+                            <p>
+                                <label>Your Email: <input type="email" name="email"/></label>
+                            </p>
+                            <p>
+                                <label>Message: <textarea name="message"></textarea></label>
+                            </p>
+                            <p>
+                                <button type="submit">Send</button>
+                            </p>
+                            </form>
+
+
+                        
         </FormContainer>
     )
 }
