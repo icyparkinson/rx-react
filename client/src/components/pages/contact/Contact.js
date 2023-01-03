64858c46d546c23280b4757da343123bc11be7fe
koulight <koulight@hotmail.com>
2023-01-03 15:44:48 -0800

    Tried new form.

--- client/src/components/pages/contact/Contact.js
+++ client/src/components/pages/contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 
 const Form = styled.form`
     display: flex;
@@ -43,7 +44,7 @@ function Contact(){
                           <button>Send Message</button>
                         </Form> */}
 
-                    <form name="rxbuddyContact" method="post">
+                    {/* <form name="rxbuddyContact" method="post">
                             <input type="hidden" name="rxBuddy-contact" value="rxbuddyContact" />
                             <p>
                                 <label>Your Name: <input type="text" name="name"/></label>
@@ -57,12 +58,41 @@ function Contact(){
                             <p>
                                 <button type="submit">Send</button>
                             </p>
-                            </form>
+                            </form> */}
 
 
-                        
+                    <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
+                        {({ handleChange, success, error }) => (
+                        <>
+                            <Honeypot />
+                            {success && <p>Thanks for contacting us!</p>}
+                            {error && (
+                            <p>Sorry, we could not reach our servers. Please try again later.</p>
+                            )}
+                            <div>
+                            <label htmlFor='name'>Name:</label>
+                            <input type='text' name='name' id='name' onChange={handleChange} />
+                            </div>
+                            <div>
+                            <label htmlFor='message'>Message:</label>
+                            <textarea
+                                type='text'
+                                name='message'
+                                id='message'
+                                rows='4'
+                                onChange={handleChange}
+                            />
+                            </div>
+                            <div>
+                            <button type='submit'>Submit</button>
+                            </div>
+                        </>
+                        )}
+                    </NetlifyForm>
         </FormContainer>
     )
 }
 
 export default Contact
+
+
