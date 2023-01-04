8e43b3c1e3a8cea3f3f88932e29734506048979b
koulight <koulight@hotmail.com>
2023-01-03 16:03:18 -0800

    Fixed formatting of form.

--- client/src/components/pages/contact/Contact.js
+++ client/src/components/pages/contact/Contact.js
@@ -33,7 +33,7 @@ function Contact(){
             <h1>Contact Icy</h1>
                         <p class="reach">If you have any comments, questions, or suggestions for improvements, please let me know!</p>
     
-                        {/* <Form target="_blank" name="rxbuddy" method="POST" data-netlify="true">
+                        <Form target="_blank" name="rxcontact" method="post">
                             <div>
                             <label for="name">Name</label>
                                 <input type="text" name="name" class="form-control" placeholder="Your Name" required />
@@ -42,56 +42,11 @@ function Contact(){
                             </div>
                                 <textarea placeholder="Your Message" class="form-control" name="message" rows="5" required></textarea>
                           <button>Send Message</button>
-                        </Form> */}
-
-                    {/* <form name="rxbuddyContact" method="post">
-                            <input type="hidden" name="rxBuddy-contact" value="rxbuddyContact" />
-                            <p>
-                                <label>Your Name: <input type="text" name="name"/></label>
-                            </p>
-                            <p>
-                                <label>Your Email: <input type="email" name="email"/></label>
-                            </p>
-                            <p>
-                                <label>Message: <textarea name="message"></textarea></label>
-                            </p>
-                            <p>
-                                <button type="submit">Send</button>
-                            </p>
-                            </form> */}
+                        </Form>
 
 
-                    {/* <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
-                        {({ handleChange, success, error }) => (
-                        <>
-                            <Honeypot />
-                            {success && <p>Thanks for contacting us!</p>}
-                            {error && (
-                            <p>Sorry, we could not reach our servers. Please try again later.</p>
-                            )}
-                            <div>
-                            <label htmlFor='name'>Name:</label>
-                            <input type='text' name='name' id='name' onChange={handleChange} />
-                            </div>
-                            <div>
-                            <label htmlFor='message'>Message:</label>
-                            <textarea
-                                type='text'
-                                name='message'
-                                id='message'
-                                rows='4'
-                                onChange={handleChange}
-                            />
-                            </div>
-                            <div>
-                            <button type='submit'>Submit</button>
-                            </div>
-                        </>
-                        )}
-                    </NetlifyForm> */}
 
-
-                    <form name="rxcontact" method="post">
+                    {/* <form name="rxcontact" method="post">
                         <input type="hidden" name="form-name" value="rxcontact" />
                         <p>
                             <label htmlFor="name">Name</label> <br />
@@ -108,7 +63,7 @@ function Contact(){
                         <p>
                             <input type="submit" value="Submit message" />
                         </p>
-                        </form>
+                        </form> */}
         </FormContainer>
     )
 }
