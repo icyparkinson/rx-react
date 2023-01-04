2fc9c761057519feef5058e27dfb274c5990492d
koulight <koulight@hotmail.com>
2023-01-04 11:52:51 -0800

    Made contact email address optional.

--- client/src/components/pages/contact/Contact.js
+++ client/src/components/pages/contact/Contact.js
@@ -39,7 +39,7 @@ function Contact(){
                             <label for="name">Name</label>
                                 <input type="text" name="name" id="name" placeholder="Your Name" required />
                             <label for="email">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Email Address" required />
+                                <input type="email" name="email" id="email" placeholder="Email Address (optional)" />
                             </div>
                                 <textarea placeholder="Your Message" id="message" name="message" rows="5" required></textarea>
                           <button>Send Message</button>
