5fff5777b1ec0c9c6ec7828bd29512359c278dc0
koulight <koulight@hotmail.com>
2023-03-16 13:50:24 -0700

    Did some changes in main. Added newFunction.

--- client/src/components/pages/daysupp/VictozaBuilder.js
+++ client/src/components/pages/daysupp/VictozaBuilder.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function VictozaBuilder(){
+
+    const newFunction = () => {
+        console.log("I want something logged here.")
+    }
+
     return(
         <div>Victoza Builder</div>
     )
